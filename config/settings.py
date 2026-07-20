baccccc521a6891fbfb52c2e7033a4efbfd40167
xmiaomiao <1833448004@qq.com>
2026-07-20 19:36:01 +0800

    告警阈值3000USTD

--- config/settings.py
+++ config/settings.py
@@ -69,7 +69,7 @@ RUN_DAILY_ON_STARTUP = os.getenv("BINANCE_RUN_DAILY_ON_STARTUP", "0") == "1"
 
 # 分钟级大额资金变动告警阈值。
 LARGE_EQUITY_CHANGE_THRESHOLDS = {
-    "USDT": float(os.getenv("LARGE_EQUITY_CHANGE_USDT_THRESHOLD", "100")),
+    "USDT": float(os.getenv("LARGE_EQUITY_CHANGE_USDT_THRESHOLD", "3000")),
     "BTC": float(os.getenv("LARGE_EQUITY_CHANGE_BTC_THRESHOLD", "1")),
 }
 
